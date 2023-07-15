3945f3c1731b908bc77d3a08bba2d6fb5bc68a4a
pingfury <pingfury@outlook.com>
2023-07-15 23:42:33 +0800

    init ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
     println!("Hello, world! rclone-gui");
+
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+
+    // Our application state:
+    let mut name = "Arthur".to_owned();
+    let mut age = 42;
+
+    eframe::run_simple_native("Rclone GUI", options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Rclone GUI");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                age += 1;
+            }
+            ui.label(format!("Hello '{name}', age {age}"));
+        });
+    })
 }
